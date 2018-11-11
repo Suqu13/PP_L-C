61ce696d25760ea876dac49d5aff3607a0a90c9d
Suqu13 <jgarstka3@gmail.com>
2018-11-11 16:04:07 +0100

    5th list

---
+++ Classes/Scala_Classes/src/main/scala/List_4/zad_5.sc
@@ -0,0 +1,23 @@
+sealed trait Graphs[A]
+case class Graph[A](succ: A => List[A]) extends Graphs[A]
+val g = Graph((i: Int) => i match {
+  case 0 => List(3)
+  case 1 => List(0, 2, 4)
+  case 2 => List(1)
+  case 3 => Nil
+  case 4 => List(0, 2)
+  case n => throw new Exception("Graph g: node " + n + " doesn't exist")
+})
+
+def depthFirstSearch[A](graph: Graph[A])(startNode: A): List[A] = {
+  def search (visited: List[A])(toVisit: List[A]): List[A] = {
+    toVisit match {
+      case Nil => Nil
+      case hd::tl => if (visited.contains(hd)) search(visited)(tl) else
+        hd::search(hd::visited)((graph succ hd)++tl)
+    }
+  }
+  search(Nil)(startNode::Nil)
+}
+
+depthFirstSearch(g)(4);
---
+++ Labs/OCaml_Labs/List_5/zad_1.ml
@@ -0,0 +1,40 @@
+type 'a nlist = Koniec | Element of 'a * ('a nlist);;
+type 'a llist = LKoniec | LElement of 'a * (unit -> 'a llist);;
+
+let podziel nl = 
+  let rec inside = function
+      (_,Koniec) -> (Koniec, Koniec)
+    | (n,Element(v, nl)) -> 
+        let nP = inside((n + 1), nl) in
+          if n mod 2 = 0 then (fst nP, Element(v, snd nP))
+          else (Element(v, fst nP), snd nP)
+  in inside((0), nl);;
+
+
+podziel (Element(5, Element(6, Element(3, Element(2, Element(1, Koniec))))));;
+podziel (Element(5, Element(3, Element(1, Koniec))));;
+podziel (Element(6, Element(2, Koniec)));;
+
+
+let rec lfrom k = LElement (k, function () -> lfrom (k+1));;
+
+let rec ltake = function
+    (0, _) -> []
+  | (_, LKoniec) -> []
+  | (n, LElement(x, xf)) -> x::ltake(n-1, xf());;
+
+let lpodziel nl = 
+  let rec odd = function
+      (_,LKoniec) -> LKoniec
+    | (n,LElement(v, nl)) -> if n mod 2 <> 0 then LElement(v, function() -> odd(n+1, nl())) 
+        else odd(n+1, nl())
+  in
+  let rec even = function
+      (_,LKoniec) -> LKoniec
+    | (n,LElement(v, nl)) -> if n mod 2 = 0 then LElement(v, function() -> even(n+1, nl())) 
+        else even(n+1, nl())
+  in (odd((0), nl), even((0), nl));;
+
+lpodziel (lfrom 20);;
+ltake (5, fst (lpodziel (lfrom 2)));; 
+ltake (5, snd (lpodziel (lfrom 2)));;
